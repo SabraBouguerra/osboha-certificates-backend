b6a1d3dcae88bc94739ec587422c3eb44ae6f1dd
Redwan <redwanfarisa@gmail.com>
2022-07-28 12:21:10 +0300

    general informations crud

--- app/Http/Controllers/API/GeneralInformationsController.php
+++ app/Http/Controllers/API/GeneralInformationsController.php
@@ -1,85 +1,95 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\API;
 
-use App\Models\general_informations;
 use Illuminate\Http\Request;
+use App\Http\Controllers\API\BaseController as BaseController;
+use App\Models\GeneralInformations;
+use Illuminate\Support\Facades\Validator;
 
-class GeneralInformationsController extends Controller
+
+
+class GeneralInfromationsController extends BaseController
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        //
+
+        $general_informations = GeneralInformations::all();
+        return $this->sendResponse($general_informations, "General Informations");
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'general_question' => 'required',
+            'summary' => 'required',
+            'user_books_id' => 'required',
+
+        ]);
+
+        if ($validator->fails()) {
+            return $this->sendError($validator->errors());
+        }
+        $input = $request->all();
+
+        try {
+            $general_informations = GeneralInformations::create($input);
+        } catch (\Illuminate\Database\QueryException $e) {
+            return $this->sendError('general_informations does not exist');
+        }
+
+        return $this->sendResponse($general_informations, "General Informations created");
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\general_informations  $general_informations
-     * @return \Illuminate\Http\Response
-     */
-    public function show(general_informations $general_informations)
+
+    public function show($id)
     {
-        //
+        $general_informations = GeneralInformations::find($id);
+
+        if (is_null($general_informations)) {
+
+            return $this->sendError('General Informations not found!');
+        }
+        return $this->sendResponse($general_informations, "General Informations");
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\general_informations  $general_informations
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(general_informations $general_informations)
+
+    public function update(Request $request,  $id)
     {
-        //
+        $input = $request->all();
+        $validator = Validator::make($request->all(), [
+            'general_question' => 'required',
+            'summary' => 'required',
+        ]);
+        if ($validator->fails()) {
+            return $this->sendError('Validation error', $validator->errors());
+        }
+
+
+        $general_informations = GeneralInformations::find($id);
+
+        $updateParam = [
+            "general_question" => $input['general_question'],
+            "summary" => $input['summary'],
+        ];
+        try {
+            $general_informations->update($updateParam);
+        } catch (\Error $e) {
+            return $this->sendError('General Informations not found');
+        }
+        return $this->sendResponse($general_informations, 'General Informations updated Successfully!');
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\general_informations  $general_informations
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, general_informations $general_informations)
+    public function destroy($id)
     {
-        //
-    }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\general_informations  $general_informations
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(general_informations $general_informations)
-    {
-        //
+        $result = GeneralInformations::destroy($id);
+
+        if ($result == 0) {
+
+            return $this->sendError('General Informations not found!');
+        }
+        return $this->sendResponse($result, 'General Informations deleted Successfully!');
     }
 }
--- app/Models/General_informations.php
+++ app/Models/General_informations.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class general_informations extends Model
+class GeneralInformations extends Model
 {
     use HasFactory;
 
