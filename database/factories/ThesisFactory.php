36bd3c0d0d27e7bf95d7a7eefdbbcef3132f7d88
saraIsmailSE <saraismailse@gmail.com>
2022-10-03 00:09:36 +0300

    لbugs

--- database/factories/ThesisFactory.php
+++ database/factories/ThesisFactory.php
@@ -15,17 +15,19 @@ class ThesisFactory extends Factory
      * @return array<string, mixed>
      */
     public function definition()
-    {
+    { 
         return [
-            'reviews'=>$this->faker->paragraph,
-            'degree'=> $this->faker->paragraph,
-            'reviewer_id',
-            'auditor_id',
-            'thesis_text',
-            'starting_page',
-            'ending_page',
-            'user_book_id'
-        
+            'thesis_text'	 => $this->faker->paragraph,
+            'starting_page'	=> strval(rand(1, 100)),
+            'ending_page'	=>  strval(rand( 100,  1000)),
+            'reviews'	=> $this->faker->randomElement([$this->faker->sentence(rand(2,3)), null]),
+            'status'	=> $this->faker->randomElement([null , 'audit', 'review', 'audited', 'rejected']),
+            'degree'	=> rand(0, 100),
+            'reviewer_id' => 1,
+            'auditor_id' => 1,
+            'user_book_id'=> 1,
+            
         ];
     }
 }
+
