a676a765e5ea46245bceaac5dd4bf88610457b07
Redwan <redwanfarisa@gmail.com>
2022-10-17 21:04:17 +0300

    thesis photos

--- database/seeders/User_BookSeeder.php
+++ database/seeders/User_BookSeeder.php
@@ -15,33 +15,33 @@ class User_BookSeeder extends Seeder
 
     public function run()
     {
-        $status = ['stage_one', 'stage_two', 'finished'];
+        $status =['review'];
 
         $users = User::all()->pluck('id')->toArray(); //all user id
-        for ($i=0; $i < count($users) ; $i++) { //for each user 
+        for ($i=0; $i < count($users) ; $i++) { //for each user
             $nBooks = rand(0,5);
             if ($nBooks > 0){
                 $books = Book::inRandomOrder()->limit($nBooks)->pluck('id')->toArray();
                 DB::table('user_book')->insert([ //last book status
                     'status' => Arr::random($status),
-                    'user_id' => $users[$i], 
+                    'user_id' => $users[$i],
                     'book_id' => $books[0]
                 ]);
-                
+
 
                 $j = count($books)-1 ;
                 while ($j > 0) { ////previous books status = finished
                     DB::table('user_book')->insert([
                         'status' => 'finished',
-                        'user_id' => $users[$i], 
+                        'user_id' => $users[$i],
                         'book_id' => $books[$j]
                     ]);
                     $j--;
                 }
             }
-        }   
-        
-    
+        }
+
+
 
 
     }
