c0b4f8515a562875b29eec23072a8620f78b07b4
redwan <redwanfarisa@gmail.com>
2023-01-15 13:54:31 -0500

    upload to server

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -30,9 +30,8 @@ class RouteServiceProvider extends ServiceProvider
 
      	$this->routes(function () {
             Route::middleware('api')
-                ->namespace($this->namespace)
-                ->prefix('api')
-                 ->group(base_path('routes/api.php'));
+            ->namespace($this->namespace)
+            ->group(base_path('routes/api.php'));
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
 
